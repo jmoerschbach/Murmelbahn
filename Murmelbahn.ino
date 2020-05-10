c2cd663aab87e36921b165e7d54cbe6c469df09f
Jonas Mörschbach <jmoerschbach@gmail.com>
2020-05-10 13:34:28 +0200

    initialer commit

---
+++ Murmelbahn.ino
@@ -0,0 +1,30 @@
+#include <FastLED.h>
+
+// How many leds in your strip?
+#define NUM_LEDS 1
+
+// For led chips like WS2812, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+// Clock pin only needed for SPI based chipsets when not using hardware SPI
+#define DATA_PIN 7
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+void setup() { 
+
+     FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);  // GRB ordering is typical
+}
+
+void loop() { 
+  // Turn the LED on, then pause
+  leds[0] = CRGB::Yellow;
+  FastLED.show();
+  delay(500);
+  // Now turn the LED off, then pause
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(500);
+
+}
---
+++ spec.d
@@ -0,0 +1,2 @@
+spec.o: \
+ D:/sloeber-workspace/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
